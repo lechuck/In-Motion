1f669a34fa73304525451200c4ba5398486699bf
Tony Karppinen <tony.karppinen@gmail.com>
2011-09-14 16:08:31 +0300

    Initial Commit

---
+++ InMotion/InMotionAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  InMotionAppDelegate.h
+//  InMotion
+//
+//  Created by Tony Karppinen on 9/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class InMotionViewController;
+
+@interface InMotionAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet InMotionViewController *viewController;
+
+@end
---
+++ InMotion/InMotionViewController.h
@@ -0,0 +1,13 @@
+//
+//  InMotionViewController.h
+//  InMotion
+//
+//  Created by Tony Karppinen on 9/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InMotionViewController : UIViewController
+
+@end
